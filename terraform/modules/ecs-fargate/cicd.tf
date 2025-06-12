87ddd233a50f685d4d56fa62b4ee9bdf70b9c056
Achyuth Nagaraj Balasubramanian <a.balasubramanian@reply.de>
2025-06-12 11:12:35 +0200

    solved error in buildspec

--- terraform/modules/ecs-fargate/cicd.tf
+++ terraform/modules/ecs-fargate/cicd.tf
@@ -131,6 +131,14 @@ resource "aws_iam_role_policy" "codebuild_policy" {
         Effect   = "Allow",
         Action   = "sts:AssumeRole",
         Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeTaskDefinition",
+          # Add any other ECS actions your CodeBuild role needs (e.g., ecs:RegisterTaskDefinition if you were doing that, but you're not)
+        ]
+        Resource = "*" # Or limit to specific task definition ARNs if desired for stricter security
       }
     ]
   })
@@ -180,6 +188,10 @@ resource "aws_codebuild_project" "app_build" {
     environment_variable {
       name  = "CONTAINER_NAME"
       value = "${var.project_name_prefix}-${var.environment_name}-webapp" # Matches container_name in task definition
+    }
+    environment_variable {
+      name  = "SERVICE_NAME"
+      value = aws_ecs_service.web_app_service.name
     }
   }
 
@@ -332,24 +344,24 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-      # Permissions for CodePipeline to access S3 for artifacts
+      # ... [your other statements for S3 and CodeBuild remain unchanged] ...
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "s3:GetObject",
           "s3:GetObjectVersion",
           "s3:PutObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:GetBucketVersioning"
         ],
         Resource = [
           aws_s3_bucket.codepipeline_artifacts.arn,
           "${aws_s3_bucket.codepipeline_artifacts.arn}/*"
         ]
       },
-      # Permissions for CodePipeline to invoke CodeBuild
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "codebuild:StartBuild",
           "codebuild:StopBuild",
           "codebuild:BatchGetBuilds",
@@ -357,15 +369,17 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
         ],
         Resource = aws_codebuild_project.app_build.arn
       },
-      # Permissions for CodePipeline to invoke CodeDeploy
+      # (MODIFIED BLOCK) Permissions for CodePipeline to invoke CodeDeploy
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "codedeploy:CreateDeployment",
           "codedeploy:GetApplication",
           "codedeploy:GetApplicationRevision",
           "codedeploy:GetDeployment",
           "codedeploy:GetDeploymentGroup",
+          # --- ADD THIS LINE ---
+          "codedeploy:RegisterApplicationRevision",
           "codedeploy:ListApplications",
           "codedeploy:ListDeploymentGroups",
           "codedeploy:ListDeployments",
@@ -375,22 +389,22 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
         ],
         Resource = [
           aws_codedeploy_app.web_app_codedeploy_app.arn,
-          aws_codedeploy_deployment_group.web_app_dg.arn
+          aws_codedeploy_deployment_group.web_app_dg.arn,
+          "arn:aws:codedeploy:${var.aws_region}:${data.aws_caller_identity.current.account_id}:deploymentconfig:CodeDeployDefault.ECSAllAtOnce"
         ]
       },
-      # Permissions for CodePipeline to retrieve the GitHub OAuth token from Secrets Manager
+      # ... [your other statements for Secrets Manager and PassRole remain unchanged] ...
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "secretsmanager:GetSecretValue",
           "secretsmanager:DescribeSecret"
         ],
-        Resource = aws_secretsmanager_secret.github_token.arn # Grant access to this specific secret
+        Resource = data.aws_secretsmanager_secret.github_token_data.arn
       },
-      # Allow CodePipeline to pass roles to other services
       {
-        Effect   = "Allow",
-        Action   = "iam:PassRole",
+        Effect = "Allow",
+        Action = "iam:PassRole",
         Resource = [
           aws_iam_role.codebuild_role.arn,
           aws_iam_role.codedeploy_role.arn
@@ -402,13 +416,26 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
 
 # 9. AWS CodePipeline
 
-resource "aws_secretsmanager_secret" "github_token" {
+## Creation for the first time
+# resource "aws_secretsmanager_secret" "github_token" {
+#   name = "${var.project_name_prefix}-${var.environment_name}-github-token"
+# }
+
+# resource "aws_secretsmanager_secret_version" "github_token_value" {
+#   secret_id     = aws_secretsmanager_secret.github_token.id
+#   secret_string = var.github_pat
+# }
+
+# Data source to retrieve the GitHub Token secret by its name
+data "aws_secretsmanager_secret" "github_token_data" {
+  # Use the same naming convention you used for the resource to retrieve it
   name = "${var.project_name_prefix}-${var.environment_name}-github-token"
 }
 
-resource "aws_secretsmanager_secret_version" "github_token_value" {
-  secret_id     = aws_secretsmanager_secret.github_token.id
-  secret_string = var.github_pat
+# Data source to retrieve the actual secret string from the latest version
+data "aws_secretsmanager_secret_version" "github_token_value_data" {
+  # Reference the ID or ARN of the secret retrieved by the previous data source
+  secret_id = data.aws_secretsmanager_secret.github_token_data.id
 }
 
 resource "aws_codepipeline" "web_app_pipeline" {
@@ -433,7 +460,7 @@ resource "aws_codepipeline" "web_app_pipeline" {
         Owner      = "nagaraj1993" # Just your username/org
         Repo       = "ecs-fargate-webapp" # Just the repo name
         Branch     = "main"
-        OAuthToken = aws_secretsmanager_secret_version.github_token_value.secret_string
+        OAuthToken = data.aws_secretsmanager_secret_version.github_token_value_data.secret_string
       }
     }
   }
@@ -460,12 +487,25 @@ resource "aws_codepipeline" "web_app_pipeline" {
       name            = "Deploy"
       category        = "Deploy"
       owner           = "AWS"
-      provider        = "CodeDeploy"
+      provider        = "CodeDeployToECS"
+      # The action needs both sets of artifacts to work
       input_artifacts = ["BuildArtifact"]
       version         = "1"
+      
       configuration = {
-        ApplicationName     = aws_codedeploy_app.web_app_codedeploy_app.name
-        DeploymentGroupName = aws_codedeploy_deployment_group.web_app_dg.deployment_group_name
+        ApplicationName                = aws_codedeploy_app.web_app_codedeploy_app.name
+        DeploymentGroupName            = aws_codedeploy_deployment_group.web_app_dg.deployment_group_name
+        
+        # This tells CodeDeploy to find taskdef.json in the SOURCE artifact. This fixes the error.
+        TaskDefinitionTemplateArtifact = "BuildArtifact"
+        TaskDefinitionTemplatePath = "taskdef.json"
+        # This tells CodeDeploy to find appspec.yml in the SOURCE artifact.
+        AppSpecTemplateArtifact        = "BuildArtifact"
+        AppSpecTemplatePath = "appspec.yaml"
+        # Point this to the BuildArtifact created by CodeBuild
+        Image1ArtifactName             = "BuildArtifact"
+        # This tells CodeDeploy WHICH container to update (must match names in appspec/taskdef)
+        Image1ContainerName            = "IMAGE1_NAME" #"${var.project_name_prefix}-${var.environment_name}-webapp"
       }
     }
   }
